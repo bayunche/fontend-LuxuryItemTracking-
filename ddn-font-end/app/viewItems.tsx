be2bfede9498dcc7004c736460aabddcce5a8c1c
jianqing.wang@ubtrobot.com <jianqing.wang@ubtrobot.com>
2024-03-26 20:21:48 +0800

    add detailtex  bug fix

--- ddn-font-end/app/viewItems.tsx
+++ ddn-font-end/app/viewItems.tsx
@@ -1,13 +1,17 @@
 import { Platform, StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
 import { SafeAreaView } from "react-native-safe-area-context";
-// import ViewItem from "E:/练习与资料/ddn font/fontend-LuxuryItemTracking-/fontend-LuxuryItemTracking-ddn-font-end/components/itemDetail.tsx";
+import ViewItem from "../components/itemDetail";
+import APPbars from "../components/AppBar";
 // import SignUpScreen from "../components/signUpScreen";
+
 export default function viewItemScreen() {
   return (
-    <SafeAreaView>
-      {/* <ViewItem></ViewItem> */}
+    <View>
+      <APPbars></APPbars>
+
+      <ViewItem></ViewItem>
       {/* <SignUpScreen></SignUpScreen> */}
-    </SafeAreaView>
+    </View>
   );
 }
--- ddn-font-end/components/itemDetail.tsx
+++ ddn-font-end/components/itemDetail.tsx
@@ -1,21 +1,71 @@
-import { View } from "./Themed";
+import { Text, View } from "./Themed";
 
 import { StyleSheet } from "react-native";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 
 import ZoomableImage from "./AnimateScroll";
+import { useFocusEffect } from "expo-router";
+import { itemInfoType, useItemStore } from "../zustand/store";
+import { getItemDetail } from "../api/item";
+import { ScrollView } from "react-native-gesture-handler";
+import { Image } from "expo-image";
+// ScrollView
+
+function itemTitle() {
+  const { setItemId, itemId, getItemInfo, itemInfo } = useItemStore(
+    (state) => ({
+      setItemId: state.setItemId,
+      itemId: state.itemId,
+      getItemInfo: state.getItemInfo,
+      itemInfo: state.itemInfo,
+    })
+  );
+}
+
 export default function ViewItem() {
+  const { setItemId, itemId, getItemInfo, itemInfo } = useItemStore(
+    (state) => ({
+      setItemId: state.setItemId,
+      itemId: state.itemId,
+      getItemInfo: state.getItemInfo,
+      itemInfo: state.itemInfo,
+    })
+  );
+  const [itemInfos, setItemInfos] = useState<itemInfoType | undefined>(
+    undefined
+  );
+  // const isMounted = useRef(false);
+  useFocusEffect(
+    useCallback(() => {
+      const params = { itemId };
+      getItemInfo(params);
+    }, [])
+  );
+  useEffect(() => {
+    setItemInfos(itemInfo);
+    console.log("set");
+  }, [itemInfo]);
+  useEffect(() => {
+    //  console.log(JSON.parse(JSON.stringify(itemInfos)))
+  }, [itemInfos]);
   return (
     <View style={styles.container}>
-      <ZoomableImage uri="https://github.com/bayunche/fontend-LuxuryItemTracking-/blob/c203917aea23a2072907d25367260ae32825eebe/ddn-font-end/assets/images/115907691_p0_master1200.jpg"></ZoomableImage>
+      {itemInfos?.itemImage && (
+        <ZoomableImage uri={itemInfo.itemImage}></ZoomableImage>
+      )}
+     
+
+      <ScrollView></ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
+    flex: 1,
+  },
+  image: {
+    height: 300,
   },
 });
